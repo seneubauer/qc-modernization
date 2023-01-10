ad6044d37bc3aa7646e24bfa9f23e5f10ed7ab1d
Scott Neubauer <scotteneubauer@gmail.com>
2023-01-10 12:12:07 -0600

    cleaned up code for cs app

--- program_clearances/solidworks_api_app/Program.cs
+++ program_clearances/solidworks_api_app/Program.cs
@@ -5,23 +5,23 @@ using System.Globalization;
 
 namespace sw_api
 {
-    // define the data object
-    public class fixture_clearance
-    {
-        public string id { get; set; }
-        public double py { get; set; }
-        public double px { get; set; }
-        public double ny { get; set; }
-        public double nx { get; set; }
-    }
+	// define the data object
+	public class fixture_clearance
+	{
+		public string? id { get; set; }
+		public double py { get; set; }
+		public double px { get; set; }
+		public double ny { get; set; }
+		public double nx { get; set; }
+	}
 
-    // define the macro class
-    public class sw_macro
-    {
-        static fixture_clearance get_fixture_clearance(int index, string file_path, string file_name, ref SldWorks app)
-        {
-            // define the null object
-            fixture_clearance null_object = new fixture_clearance
+	// define the macro class
+	public class sw_macro
+	{
+		static fixture_clearance get_fixture_clearance(string file_path, string file_name, ref SldWorks app)
+		{
+			// define the null object
+			fixture_clearance null_object = new fixture_clearance
 			{
 				id = file_name,
 				py = 0,
@@ -30,7 +30,7 @@ namespace sw_api
 				nx = 0
 			};
 
-            // return nothing if the file doesn't exist
+			// return nothing if the file doesn't exist
 			if (!File.Exists(file_path))
 				return null_object;
 			
@@ -63,32 +63,51 @@ namespace sw_api
 					return null_object;
 
 				// extract the sensors from their features
-				Sensor py = (Sensor)py_feature.GetSpecificFeature2();
-				Sensor px = (Sensor)px_feature.GetSpecificFeature2();
-				Sensor ny = (Sensor)ny_feature.GetSpecificFeature2();
-				Sensor nx = (Sensor)nx_feature.GetSpecificFeature2();
-
+				Sensor? py = null;
+				Sensor? px = null;
+				Sensor? ny = null;
+				Sensor? nx = null;
+				if (py_feature != null) { py = (Sensor)py_feature.GetSpecificFeature2(); }
+				if (px_feature != null) { px = (Sensor)px_feature.GetSpecificFeature2(); }
+				if (ny_feature != null) { ny = (Sensor)ny_feature.GetSpecificFeature2(); }
+				if (nx_feature != null) { nx = (Sensor)nx_feature.GetSpecificFeature2(); }
+				
 				// extract the sensor data
-				DimensionSensorData py_data = (DimensionSensorData)py.GetSensorFeatureData();
-				DimensionSensorData px_data = (DimensionSensorData)px.GetSensorFeatureData();
-				DimensionSensorData ny_data = (DimensionSensorData)ny.GetSensorFeatureData();
-				DimensionSensorData nx_data = (DimensionSensorData)nx.GetSensorFeatureData();
+				DimensionSensorData? py_data = null;
+				DimensionSensorData? px_data = null;
+				DimensionSensorData? ny_data = null;
+				DimensionSensorData? nx_data = null;
+				if (py != null) { py_data = (DimensionSensorData)py.GetSensorFeatureData(); }
+				if (px != null) { px_data = (DimensionSensorData)px.GetSensorFeatureData(); }
+				if (ny != null) { ny_data = (DimensionSensorData)ny.GetSensorFeatureData(); }
+				if (nx != null) { nx_data = (DimensionSensorData)nx.GetSensorFeatureData(); }
 
 				// create the output object
-				fixture_clearance output = new fixture_clearance
+				if (py_data != null && px_data != null && ny_data != null && nx_data != null)
 				{
-					id = file_name,
-					py = py_data.SensorValue,
-					px = px_data.SensorValue,
-					ny = ny_data.SensorValue,
-					nx = nx_data.SensorValue
-				};
+					fixture_clearance output = new fixture_clearance
+					{
+						id = file_name,
+						py = py_data.SensorValue,
+						px = px_data.SensorValue,
+						ny = ny_data.SensorValue,
+						nx = nx_data.SensorValue
+					};
 
-				// release the resources
-				app.CloseDoc(file_name);
+					// release the resources
+					app.CloseDoc(file_name + ".sldprt");
 
-				// return the results
-				return output;
+					// return the results
+					return output;
+				}
+				else
+				{
+					// release the resources
+					app.CloseDoc(file_name + ".sldprt");
+
+					// return the null result
+					return null_object;
+				}
 			}
 			catch (NullReferenceException e)
 			{
@@ -96,66 +115,73 @@ namespace sw_api
 				Console.WriteLine($"{file_name}: {e.Message}");
 
 				// release the resources
-				app.CloseDoc(file_name);
+				app.CloseDoc(file_name + ".sldprt");
 
 				// return null results
 				return null_object;
 			}
-        }
+		}
 
-        // define logic loop
-        static void Main(string[] args)
-        {
-            // interpret the initial arguments
-            string root_dir = args[0];
-            string path_dir = args[1];
-            string output_dir = args[2];
+		// define logic loop
+		static void Main(string[] args)
+		{
+			// interpret the initial arguments
+			string root_dir = args[0];
+			string path_dir = args[1];
+			string output_dir = args[2];
 
-            // initialize the solidworks application
-            SldWorks app = (SldWorks)Activator.CreateInstance(Type.GetTypeFromProgID("SldWorks.Application.30"));
+			// initialize the solidworks application
+			Type? app_type = Type.GetTypeFromProgID("SldWorks.Application.30");
+			SldWorks? app = null;
+			if (app_type != null) { app = (SldWorks?)Activator.CreateInstance(app_type); }
 
-            int i = 0;
-            List<fixture_clearance> data = new List<fixture_clearance>();
-            string[] fixture_folders = Directory.GetDirectories(root_dir);
-            foreach (var dir in fixture_folders)
-            {
-                FileAttributes attr = File.GetAttributes(dir);
-				if (attr.HasFlag(FileAttributes.Directory))
-				{
-					var file_names = Directory.GetFiles(dir).Where(name => name.ToLower().EndsWith(".lnk")).Where(name => !name.Contains("-")).ToList();
-					if (file_names.Count > 0)
-					{
-						var file_name = Path.GetFileName(file_names[0]).Split(".")[0];
-						var file_path = Path.Join(path_dir, file_name, file_name + ".sldprt");
-						data.Add(get_fixture_clearance(i, file_path, file_name, ref app));
-
-						i++;
-						Console.WriteLine($"{file_name}");
-					}
-				}
-            }
-
-			// close the solidworks application
-			app.CloseAllDocuments(true);
-			app.ExitApp();
-			
-			// write the list of fixture clearances to a csv
-			using (var sw = new StreamWriter(output_dir))
+			// only run the rest of the script if the solidworks application was properly started
+			if (app != null)
 			{
-				using (var csv = new CsvWriter(sw, CultureInfo.InvariantCulture))
+				List<fixture_clearance> data = new List<fixture_clearance>();
+				string[] fixture_folders = Directory.GetDirectories(root_dir);
+				foreach (var dir in fixture_folders)
 				{
-					csv.WriteHeader<fixture_clearance>();
-					csv.NextRecord();
-					foreach (var record in data)
+					FileAttributes attr = File.GetAttributes(dir);
+					if (attr.HasFlag(FileAttributes.Directory))
 					{
-						csv.WriteRecord(record);
-						csv.NextRecord();
+						var file_names = Directory.GetFiles(dir).Where(name => name.ToLower().EndsWith(".lnk")).Where(name => !name.Contains("-")).ToList();
+						if (file_names.Count > 0)
+						{
+							var file_name = Path.GetFileName(file_names[0]).Split(".")[0];
+							var file_path = Path.Join(path_dir, file_name, file_name + ".sldprt");
+							data.Add(get_fixture_clearance(file_path, file_name, ref app));
+							Console.WriteLine($"{file_name}");
+						}
 					}
 				}
-			}
 
-			// signal program end
-			Console.WriteLine("End of program.");
-        }
-    }
+				// close the solidworks application
+				app.CloseAllDocuments(true);
+				app.ExitApp();
+				
+				// write the list of fixture clearances to a csv
+				using (var sw = new StreamWriter(output_dir))
+				{
+					using (var csv = new CsvWriter(sw, CultureInfo.InvariantCulture))
+					{
+						csv.WriteHeader<fixture_clearance>();
+						csv.NextRecord();
+						foreach (var record in data)
+						{
+							csv.WriteRecord(record);
+							csv.NextRecord();
+						}
+					}
+				}
+
+				// signal program end
+				Console.WriteLine("End of program.");
+			}
+			else
+			{
+				Console.WriteLine("Solidworks application initiated as null.");
+			}
+		}
+	}
 }
